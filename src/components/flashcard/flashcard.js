39423d0b8225da565dfceeda5ee196449b0dc5e2
rhc822 <rhc822@gmail.com>
2019-12-11 16:43:20 -0600

    Created a simple hard-coded flashcard

---
+++ src/components/flashcard/flashcard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+class Flashcard extends Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="card-content">
+          <h1>Word</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Flashcard;
