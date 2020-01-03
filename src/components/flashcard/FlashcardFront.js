0c3332f57e4973d70934ed136c95b646ca11f59a
rhc822 <rhc822@gmail.com>
2020-01-03 08:35:45 -0600

    Did simple styling for front of card

--- src/components/flashcard/FlashcardFront.js
+++ src/components/flashcard/FlashcardFront.js
@@ -25,22 +25,28 @@ state = {
     console.log(this.state)
       return (
         <>
-          <h1>{this.state.word}</h1>
-          <button
-            type="button"
-            onClick={() => { this.props.history.push(`/flashcard/${this.props.flashcardId}/FlashcardEdit`) }}
-          >Edit
-          </button>
-          <button
-            type="button"
-            onClick={() => this.props.deleteFlashcard(this.props.flashcardId)}
-          >Delete
-          </button>
-          <Link
-            className="card-flip"
-            to={`/flashcard/${this.props.flashcardId}/FlashcardBack`}
-          >Flip
-          </Link>
+          <div className="w3-panel w3-margin w3-card-2 w3-center w3-display-middle">
+            <h1>{this.state.word}</h1>
+            <br />
+            <button
+              className="w3-margin"
+              type="button"
+              onClick={() => { this.props.history.push(`/flashcard/${this.props.flashcardId}/FlashcardEdit`) }}>
+                Edit
+            </button>
+            <button
+              className="w3-margin"
+              type="button"
+              onClick={() => this.props.deleteFlashcard(this.props.flashcardId)}>
+                Delete
+            </button>
+            <br />
+            <Link
+              className="card-flip"
+              to={`/flashcard/${this.props.flashcardId}/FlashcardBack`}>
+                Flip
+            </Link>
+          </div>
         </>
     );
   }
