d941eaa3dd4475648e9e1174ff41ef1c6b77fc68
rhc822 <rhc822@gmail.com>
2020-01-05 14:53:55 -0600

    Tweaked display of the flashcards on main screen

--- src/components/flashcard/Flashcard.js
+++ src/components/flashcard/Flashcard.js
@@ -6,16 +6,13 @@ import './Flashcard.css'
 class Flashcard extends Component {
   render() {
       return (
-    <div
-      className="card"
-      onClick= {() =>
-      { this.props.history.push(`/flashcard/${this.props.id}`)}
-      }>
         <div
-          className="w3-margin w3-card-2 w3-left">
-          <h1>{this.props.word}</h1>
+          className="w3-margin w3-card-2 w3-left"
+          onClick= {() =>
+            { this.props.history.push(`/flashcard/${this.props.id}`)}
+            }>
+          <h1 className="w3-margin">{this.props.word}</h1>
         </div>
-      </div>
     );
   }
 }
